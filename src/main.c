bff463f62bf9fa861edf4ad2b1e613ebe69fc675
abiralneupane2 <abiralneupane2@gmail.com>
2022-07-07 20:47:39 +0545

    rotate

--- src/main.c
+++ src/main.c
@@ -1,21 +1,58 @@
 #include <gtk/gtk.h>
 #include <epoxy/gl.h>
+#include <cglm/cglm.h>
+#include <time.h>
+
+#include <pthread.h>
 
 static GtkWidget *gl_area = NULL;
 static GLuint program_vbo;
 static GLuint program;
+pthread_t thread_id;
+static GLuint mvp_location;
+
+static float angle=0.f;
+static vec3 pos={0.f,0.f};
 GLint err;
+
+
 
 static const GLfloat vertices[] = {
     -0.5f, -0.5f, 0.0f,
     0.5f, -0.5f, 0.0f,
     0.0f, 0.5f, 0.0f};
 
+void delay(int milli_seconds)
+{
+    // Converting time into milli_seconds
+
+
+    // Storing start time
+    clock_t start_time = clock();
+
+    // looping till required time is not achieved
+    while (clock() < start_time + milli_seconds);
+}
+
+void *rotate(){
+    printf("here");
+    delay(2000);
+    while(1){
+        delay(1000);
+        if(angle==360.f)
+        angle = 0.f;
+        angle+=0.05f;
+
+        gtk_widget_queue_draw (gl_area);
+    }
+}
+
 static const char *vertex_shader =
 "#version 330\n"
 "layout(location = 0) in vec4 position;\n"
+"uniform mat4 MVP;\n"
 "void main() {\n"
-"gl_Position = position;\n"
+"gl_Position = MVP * position;\n"
 "}";
 
 static const char *fragment_shader =
@@ -26,30 +63,40 @@ static const char *fragment_shader =
 "outputColor = mix(vec4(1.0f, 0.85f, 0.35f, 1.0f), vec4(0.2f, 0.2f, 0.2f, 1.0f), lerpVal);\n"
 "}";
 
+
 static gboolean
 render(GtkGLArea *area,
        GdkGLContext *context)
 {
     if (gtk_gl_area_get_error(area) != NULL)
         return FALSE;
+    mat4 m={0};
+    vec3 v = {0.f, 0.f, 1.f};
+    
+    glm_rotate_make(m, angle, v);
+
+
+    // for(int i=0;i<sizeof(mvp);i++){
+    //     printf("%f\n", mvp[i]);
+    // }
 
     /* Clear the viewport */
-    glClearColor(0.9, 0.9, 0.9, 1.0);
+    delay(500);
+    glClearColor(0.f, 0.f, 0.f, 1.0);
     glClear(GL_COLOR_BUFFER_BIT);
     glUseProgram(program);
+
+    glUniformMatrix4fv (mvp_location, 1, GL_FALSE, &m[0][0]);
     glBindBuffer(GL_ARRAY_BUFFER, program_vbo);
     
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
     // glBufferSubData(GL_ARRAY_BUFFER, 0, 2*sizeof(vertices), vertices);
 
-    err = glGetError();
-    printf("%d\n", err);
+    
     /* Draw our object */
     glDrawArrays(GL_TRIANGLES, 0, 3);
 
-    err = glGetError();
-    printf("%d\n", err);
 
     /* We finished using the buffers and program */
     glDisableVertexAttribArray(0);
@@ -57,7 +104,6 @@ render(GtkGLArea *area,
     glUseProgram(0);
     /* Flush the contents of the pipeline */
     glFlush();
-
     return TRUE;
 }
 
@@ -75,6 +121,7 @@ void realize(GtkWidget *widget)
     if (gtk_gl_area_get_error (GTK_GL_AREA (widget)) != NULL)
         return;
     GLuint vbo, vao;
+    GLuint mvp = 0;
     int status;
     glGenVertexArrays (1, &vao);
     glBindVertexArray (vao);
@@ -118,17 +165,24 @@ void realize(GtkWidget *widget)
         goto out;
     }
 
+    mvp = glGetUniformLocation (program, "MVP");
+    mvp_location = mvp;
     glDetachShader(program, v_shader);
     glDetachShader(program, f_shader);
 
 out:
     glDeleteShader(v_shader);
     glDeleteShader(f_shader);
+
+    
+    pthread_create(&thread_id, NULL, rotate, NULL);
+
+    
 }
 
 void activate(GtkApplication *app)
 {
-    GtkWidget *window, *box;
+    GtkWidget *window, *box, *button;
     window = gtk_application_window_new(app);
     gtk_window_set_title(GTK_WINDOW(window), "OpenGL Area");
     gtk_window_set_default_size(GTK_WINDOW(window), 800, 600);
@@ -156,6 +210,11 @@ void activate(GtkApplication *app)
 
     /* The main "draw" call for GtkGLArea */
     g_signal_connect(gl_area, "render", G_CALLBACK(render), NULL);
+
+    button = gtk_button_new_with_label ("Quit");
+    gtk_widget_set_hexpand (button, TRUE);
+    gtk_box_append (GTK_BOX (box), button);
+    g_signal_connect_swapped (button, "clicked", G_CALLBACK (gtk_window_destroy), window);
 
     gtk_widget_show(window);
 }
